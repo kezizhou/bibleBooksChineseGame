19b29a65f5e5e1641e61405aec67ef8b92dcee51
kezizhou <kezizhou@gmail.com>
2020-07-12 09:56:21 -0400

    Adding initial statistics page

--- bibleBooksWPF/Settings.xaml.cs
+++ bibleBooksWPF/Settings.xaml.cs
@@ -2,7 +2,6 @@
 using System.Windows.Controls;
 using System.Deployment.Application;
 using System;
-using System.Windows.Forms;
 
 namespace BibleBooksWPF {
 	/// <summary>
@@ -14,20 +13,21 @@ namespace BibleBooksWPF {
 		}
 
 		private void Page_Loaded(object sender, RoutedEventArgs e) {
+			// Previous audio setting
+			if (Properties.Settings.Default.blnAudio) {
+				radAudioOn.IsChecked = true;
+				radAudioOff.IsChecked = false;
+			} else {
+				radAudioOn.IsChecked = false;
+				radAudioOff.IsChecked = true;
+			}
+
+			// Current version
 			if (ApplicationDeployment.IsNetworkDeployed) {
 				Version version = ApplicationDeployment.CurrentDeployment.CurrentVersion;
 				lblVersion.Text = string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
 			} else {
 				lblVersion.Text = "Not Installed";
-			}
-		}
-
-		private void RadAudio_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e) {
-			if (radAudioOn.IsChecked == true) {
-				App.Current.Properties["blnAudio"] = true;
-			}
-			else {
-				App.Current.Properties["blnAudio"] = false;
 			}
 		}
 
@@ -46,15 +46,15 @@ namespace BibleBooksWPF {
 
 				}
 				catch (DeploymentDownloadException dde) {
-					System.Windows.MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
+					MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
 					return;
 				}
 				catch (InvalidDeploymentException ide) {
-					System.Windows.MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
+					MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
 					return;
 				}
 				catch (InvalidOperationException ioe) {
-					System.Windows.MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
+					MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
 					return;
 				}
 
@@ -62,14 +62,14 @@ namespace BibleBooksWPF {
 					Boolean doUpdate = true;
 
 					if (!info.IsUpdateRequired) {
-						MessageBoxResult msgResult = System.Windows.MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButton.OKCancel);
+						MessageBoxResult msgResult = MessageBox.Show("An update is available. Would you like to update the application now?", "Update Available", MessageBoxButton.OKCancel);
 						if (!(MessageBoxResult.OK == msgResult)) {
 							doUpdate = false;
 						}
 					}
 					else {
 						// Display a message that the app MUST reboot. Display the minimum required version.
-						System.Windows.MessageBox.Show( "This application has detected a mandatory update from your current " +
+						MessageBox.Show( "This application has detected a mandatory update from your current " +
 							"version to version " + info.MinimumRequiredVersion.ToString() +
 							". The application will now install the update and restart.",
 							"Update Available", MessageBoxButton.OK,
@@ -79,22 +79,22 @@ namespace BibleBooksWPF {
 					if (doUpdate) {
 						try {
 							ad.Update();
-							System.Windows.MessageBox.Show("The application has been upgraded, and will now restart.");
+							MessageBox.Show("The application has been upgraded, and will now restart.");
 							System.Windows.Forms.Application.Restart();
-							System.Windows.Application.Current.Shutdown();
+							Application.Current.Shutdown();
 						}
 						catch (DeploymentDownloadException dde) {
-							System.Windows.MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
+							MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
 							return;
 						}
 					}
 				} else {
 					// No updates
-					System.Windows.MessageBox.Show("There are no new updates.");
+					MessageBox.Show("There are no new updates.");
 				}
 			} else {
 				// Program is not installed, debug mode
-				System.Windows.MessageBox.Show("The program has not been installed on your computer. Please try and re-install the application.");
+				MessageBox.Show("The program has not been installed on your computer. Please try and re-install the application.");
 				return;
 			}
 		}
@@ -125,7 +125,15 @@ namespace BibleBooksWPF {
 		}
 
 		private void ImenExit_Click(object sender, RoutedEventArgs e) {
-			System.Windows.Application.Current.Shutdown();
+			Application.Current.Shutdown();
+		}
+
+		private void RadAudioOff_Checked(object sender, RoutedEventArgs e) {
+			Properties.Settings.Default.blnAudio = false;
+		}
+
+		private void RadAudioOn_Checked(object sender, RoutedEventArgs e) {
+			Properties.Settings.Default.blnAudio = true;
 		}
 	}
 }
