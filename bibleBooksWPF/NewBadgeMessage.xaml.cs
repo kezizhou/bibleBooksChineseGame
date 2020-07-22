3a27c72e51b400b8505fc89851e6ef18f07f3f0a
kezizhou <kezizhou@gmail.com>
2020-07-22 18:52:15 -0400

    Adding badges

---
+++ bibleBooksWPF/NewBadgeMessage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace BibleBooksWPF {
+	/// <summary>
+	/// Interaction logic for NewBadgeMessage.xaml
+	/// </summary>
+	public partial class NewBadgeMessage : Window {
+		public NewBadgeMessage() {
+			InitializeComponent();
+		}
+
+		private void BtnOK_Click(object sender, RoutedEventArgs e) {
+			this.Close();
+		}
+	}
+}
