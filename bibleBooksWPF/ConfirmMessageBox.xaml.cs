871591ba6c120220ffe89899f8cf2e5bc199353c
Keziah Zhou <kezizhou@gmail.com>
2020-08-04 16:32:51 -0400

    Adding main language setting and confirm delete

---
+++ bibleBooksWPF/ConfirmMessageBox.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+
+namespace BibleBooksWPF {
+	/// <summary>
+	/// Interaction logic for ConfirmMessageBox.xaml
+	/// </summary>
+	public partial class ConfirmMessageBox : Window {
+		public ConfirmMessageBox() {
+			InitializeComponent();
+		}
+
+		public string strMsgReturn {
+			get;
+			set;
+		}
+
+		private void btnYes_Click(object sender, RoutedEventArgs e) {
+			strMsgReturn = "Yes";
+			this.Close();
+		}
+
+		private void btnCancel_Click(object sender, RoutedEventArgs e) {
+			strMsgReturn = "Cancel";
+			this.Close();
+		}
+	}
+}
