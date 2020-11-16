21cfa3a5ca36afb092fff39613f861449c8f87dd
Keziah Zhou <kezizhou@gmail.com>
2020-11-16 10:45:09 -0500

    Adding Bible book control

---
+++ bibleBooksWPF/Models/BibleBook.cs
@@ -0,0 +1,18 @@
+﻿
+namespace BibleBooksWPF.Models {
+	public class BibleBook {
+		public string strEnglish { get; set; }
+		public string strChinese { get; set; }
+		public double dblTranslateTransformX { get; set; } = 0;
+		public double dblTranslateTransformY { get; set; } = 0;
+
+		public BibleBook(string strEnglish, string strChinese) {
+			this.strEnglish = strEnglish;
+			this.strChinese = strChinese;
+		}
+
+		public BibleBook() {
+
+		}
+	}
+}
